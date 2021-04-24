dea376f1545a465407d35a51e25adbb6bb68ff00
Jafet Aaron Garcia <contact@jafetg.com>
2021-04-24 21:10:35 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -32,10 +32,28 @@ function ternaryTeenager(age) {
 
 function switchAge(age) {
   switch(age){
-    case (age >= 13 && age <= 19 ):
+    case 13:
+      console.log("You are a teenager");
+      break;
+    case 14:
+      console.log("You are a teenager");
+      break;
+    case 15:
+      console.log("You are a teenager");
+      break;
+    case 16:
+      console.log("You are a teenager");
+      break;
+    case 17:
+      console.log("You are a teenager");
+      break;
+    case 18:
+      console.log("You are a teenager");
+      break;
+    case 19:
       console.log("You are a teenager");
       break;
     default:
-      console.log("you have an age");
+      return "you have an age";
     }
 }
